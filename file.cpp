eceaf82e5dab98f47aa1905a4665371b21c0cb97
___mr.abhishek <asharma0269@gmail.com>
2019-04-22 18:48:05 +0530

    commit4

--- file.cpp
+++ file.cpp
@@ -127,4 +127,90 @@ public:
 	{cout<<"\t"<<admno<<setw(20)<<name<<setw(10)<<token<<endl;}
 
 };         //class ends here
+//***************************************************************
+//    	global declaration for stream object, object
+//****************************************************************
+
+fstream fp,fp1;
+book bk;
+student st;
+
+
+//***************************************************************
+//    	function to write in file
+//****************************************************************
+
+void write_book()
+{
+	char ch;
+	fp.open("book.dat",ios::out|ios::app);
+	do
+	{
+		clrscr();
+		bk.create_book();
+		fp.write((char*)&bk,sizeof(book));
+		cout<<"\n\nDo you want to add more record..(y/n?)";
+		cin>>ch;
+	}while(ch=='y'||ch=='Y');
+	fp.close();
+}
+
+void write_student()
+{
+	char ch;
+	fp.open("student.dat",ios::out|ios::app);
+	do
+	{
+		st.create_student();
+		fp.write((char*)&st,sizeof(student));
+		cout<<"\n\ndo you want to add more record..(y/n?)";
+		cin>>ch;
+	}while(ch=='y'||ch=='Y');
+	fp.close();
+}
+
+
+//***************************************************************
+//    	function to read specific record from file
+//****************************************************************
+
+
+void display_spb(char n[])
+{
+	cout<<"\nBOOK DETAILS\n";
+	int flag=0;
+	fp.open("book.dat",ios::in);
+	while(fp.read((char*)&bk,sizeof(book)))
+	{
+		if(strcmpi(bk.retbno(),n)==0)
+		{
+			bk.show_book();
+		 	flag=1;
+		}
+	}
 	
+	fp.close();
+	if(flag==0)
+		cout<<"\n\nBook does not exist";
+	getch();
+}
+
+void display_sps(char n[])
+{
+	cout<<"\nSTUDENT DETAILS\n";
+	int flag=0;
+	fp.open("student.dat",ios::in);
+	while(fp.read((char*)&st,sizeof(student)))
+	{
+		if((strcmpi(st.retadmno(),n)==0))
+		{
+			st.show_student();
+			flag=1;
+		}
+	}
+	
+	fp.close();
+	if(flag==0)
+    		cout<<"\n\nStudent does not exist";
+ 	getch();
+}	
