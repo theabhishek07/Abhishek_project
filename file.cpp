ecdd889f065c8e3cb6afa655566bc24f1fe5400a
___mr.abhishek <asharma0269@gmail.com>
2019-04-22 18:46:34 +0530

    commit2

--- file.cpp
+++ file.cpp
@@ -32,3 +32,32 @@ class book
 		gets(aname);
 		cout<<"\n\n\nBook Created..";
 	}
+void show_book()
+	{
+		cout<<"\nBook no. : "<<bno;
+		cout<<"\nBook Name : ";
+		puts(bname);
+		cout<<"Author Name : ";
+		puts(aname);
+	}
+
+	void modify_book()
+	{
+		cout<<"\nBook no. : "<<bno;
+		cout<<"\nModify Book Name : ";
+		gets(bname);
+		cout<<"\nModify Author's Name of Book : ";
+		gets(aname);
+	}
+
+	char* retbno()
+	{
+		return bno;
+	}
+
+	void report()
+	{cout<<bno<<setw(30)<<bname<<setw(30)<<aname<<endl;}
+
+
+};         //class ends here
+	
