ed805b46ef4e9b024645184f28639eb19e21b9fc
___mr.abhishek <asharma0269@gmail.com>
2019-04-22 18:45:57 +0530

    commit1

---
+++ file.cpp
@@ -0,0 +1,34 @@
+//***************************************************************
+//                   HEADER FILE USED IN PROJECT
+//****************************************************************
+
+#include<fstream.h>
+#include<conio.h>
+#include<stdio.h>
+#include<process.h>
+#include<string.h>
+#include<iomanip.h>
+
+//***************************************************************
+//                   CLASS USED IN PROJECT
+//****************************************************************
+
+
+
+class book
+{
+	char bno[6];
+	char bname[50];
+	char aname[20];
+  public:
+	void create_book()
+	{
+		cout<<"\nNEW BOOK ENTRY...\n";
+		cout<<"\nEnter The book no.";
+		cin>>bno;
+		cout<<"\n\nEnter The Name of The Book ";
+		gets(bname);
+		cout<<"\n\nEnter The Author's Name ";
+		gets(aname);
+		cout<<"\n\n\nBook Created..";
+	}
