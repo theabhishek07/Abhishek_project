a330f882834437092799821d95c671f13387c9ce
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2019-04-22 19:09:23 +0530

    commit1

---
+++ file1.cpp
@@ -0,0 +1,23 @@
+//***************************************************************
+//    	INTRODUCTION FUNCTION
+//****************************************************************
+
+void intro()
+{
+	clrscr();
+	gotoxy(35,11);
+	cout<<"LIBRARY";
+	gotoxy(35,14);
+	cout<<"MANAGEMENT";
+	gotoxy(35,17);
+	cout<<"SYSTEM";
+	cout<<"\n\nMADE BY : YOUR NAME";
+	cout<<"\n\nSCHOOL : SCHOOL NAME";
+	getch();
+}
+
+
+
+//***************************************************************
+//    	ADMINISTRATOR MENU FUNCTION
+//****************************************************************
