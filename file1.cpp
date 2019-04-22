80c8e523b6ca6ef2e17fcdc7368f1b16c82aa44b
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2019-04-22 19:10:07 +0530

    commit2

--- file1.cpp
+++ file1.cpp
@@ -21,3 +21,54 @@ void intro()
 //***************************************************************
 //    	ADMINISTRATOR MENU FUNCTION
 //****************************************************************
+
+void admin_menu()
+{
+	clrscr();
+	int ch2;
+	cout<<"\n\n\n\tADMINISTRATOR MENU";
+	cout<<"\n\n\t1.CREATE STUDENT RECORD";
+	cout<<"\n\n\t2.DISPLAY ALL STUDENTS RECORD";
+	cout<<"\n\n\t3.DISPLAY SPECIFIC STUDENT RECORD ";
+	cout<<"\n\n\t4.MODIFY STUDENT RECORD";
+	cout<<"\n\n\t5.DELETE STUDENT RECORD";
+	cout<<"\n\n\t6.CREATE BOOK ";
+	cout<<"\n\n\t7.DISPLAY ALL BOOKS ";
+	cout<<"\n\n\t8.DISPLAY SPECIFIC BOOK ";
+	cout<<"\n\n\t9.MODIFY BOOK ";
+	cout<<"\n\n\t10.DELETE BOOK ";
+	cout<<"\n\n\t11.BACK TO MAIN MENU";
+	cout<<"\n\n\tPlease Enter Your Choice (1-11) ";
+	cin>>ch2;
+	switch(ch2)
+	{
+    		case 1: clrscr();
+	    		write_student();break;
+    		case 2: display_alls();break;
+    		case 3:
+	       		char num[6];
+	       		clrscr();
+	       		cout<<"\n\n\tPlease Enter The Admission No. ";
+	       		cin>>num;
+	       		display_sps(num);
+	       		break;
+      		case 4: modify_student();break;
+      		case 5: delete_student();break;
+		case 6: clrscr();
+			write_book();break;
+		case 7: display_allb();break;
+		case 8: {
+	       		char num[6];
+	       		clrscr();
+	       		cout<<"\n\n\tPlease Enter The book No. ";
+	       		cin>>num;
+	       		display_spb(num);
+	       		break;
+			}
+      		case 9: modify_book();break;
+      		case 10: delete_book();break;
+     		case 11: return;
+      		default:cout<<"\a";
+   	}
+   	admin_menu();
+}
