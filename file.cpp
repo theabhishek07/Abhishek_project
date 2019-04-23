46c827d9ce86f3f9223e32c9b0819533cbc110a9
Bhavini Mishra <mily.mishra01@gmail.com>
2019-04-23 23:56:03 +0530

    commit1

--- file.cpp
+++ file.cpp
@@ -213,4 +213,32 @@ void display_sps(char n[])
 	if(flag==0)
     		cout<<"\n\nStudent does not exist";
  	getch();
-}	
+}
+//***************************************************************
+//    	function to modify record of file
+//****************************************************************
+
+
+void modify_book()
+{
+	char n[6];
+	int found=0;
+	clrscr();
+	cout<<"\n\n\tMODIFY BOOK REOCORD.... ";
+	cout<<"\n\n\tEnter The book no. of The book";
+	cin>>n;
+	fp.open("book.dat",ios::in|ios::out);
+	while(fp.read((char*)&bk,sizeof(book)) && found==0)
+	{
+		if(strcmpi(bk.retbno(),n)==0)
+		{
+			bk.show_book();
+			cout<<"\nEnter The New Details of book"<<endl;
+			bk.modify_book();
+			int pos=-1*sizeof(bk);
+		    	fp.seekp(pos,ios::cur);
+		    	fp.write((char*)&bk,sizeof(book));
+		    	cout<<"\n\n\t Record Updated";
+		    	found=1;
+		}
+	}
