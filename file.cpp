e374f627aaa43ead7ac2478ed7508d378aae59a8
Bhavini Mishra <mily.mishra01@gmail.com>
2019-04-24 00:23:28 +0530

    commit3

--- file.cpp
+++ file.cpp
@@ -280,3 +280,33 @@ void modify_student()
 		cout<<"\n\n Record Not Found ";
 	getch();
 }
+//***************************************************************
+//    	function to delete record of file
+//****************************************************************
+
+
+void delete_student()
+{
+	char n[6];
+	int flag=0;
+	clrscr();
+    	cout<<"\n\n\n\tDELETE STUDENT...";
+    	cout<<"\n\nEnter The admission no. of the Student You Want To Delete : ";
+    	cin>>n;
+    	fp.open("student.dat",ios::in|ios::out);
+    	fstream fp2;
+    	fp2.open("Temp.dat",ios::out);
+    	fp.seekg(0,ios::beg);
+    	while(fp.read((char*)&st,sizeof(student)))
+	{
+		if(strcmpi(st.retadmno(),n)!=0)
+	     		fp2.write((char*)&st,sizeof(student));
+		else
+	   		flag=1;
+	}
+
+	fp2.close();
+    	fp.close();
+    	remove("student.dat");
+    	rename("Temp.dat","student.dat");
+    	if(flag==1)
