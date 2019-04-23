fc09167a95441ba475a54b8189c1a5f225d9b52f
Aakarshit agarwal <46739754+AakarshitAgarwal@users.noreply.github.com>
2019-04-24 03:14:12 +0530

    Update file.cpp
    commit 1

--- file.cpp
+++ file.cpp
@@ -475,3 +475,22 @@ void book_deposit()
   fp.close();
   fp1.close();
   }
+  //***************************************************************
+//    	INTRODUCTION FUNCTION
+//****************************************************************
+
+void intro()
+{
+	clrscr();
+	gotoxy(35,11);
+	cout<<"LIBRARY";
+	gotoxy(35,14);
+	cout<<"MANAGEMENT";
+	gotoxy(35,17);
+	cout<<"SYSTEM";
+	cout<<"\n\nMADE BY : YOUR NAME";
+	cout<<"\n\nSCHOOL : SCHOOL NAME";
+	getch();
+}
+
+
